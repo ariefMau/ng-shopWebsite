5ec3db036bf94414dce6da2ece311e58df994f16
ariefMau <ariefmaulana518@gmail.com>
2021-06-06 13:20:46 +0700

    design layout produk & API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import Hasil from "./components/Hasil";
+import ListCategories from "./components/ListCategories";
+import NavbarComponent from "./components/NavbarComponent";
+import { API_URL } from "./utils/Constants";
+import axios from "axios";
+import Menus from "./components/Menus";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+    .get(API_URL + "products")
+    .then((res) => {
+      console.log("Response : ", res);
+      const menus = res.data;
+      this.setState({ menus });
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  render() {
+    const {menus} = this.state
+    return (
+      <div className="App">
+        <NavbarComponent />
+        <div className="mt-5">
+          <Container fluid>
+            <Row>
+              <ListCategories />
+              <Col>
+                <h5><b>Daftar Produk</b></h5>
+                <hr />
+                <Row>
+                  {
+                    menus && menus.map((menu) => (
+                      <Menus
+                        key={menu.id}
+                        menu={menu}   
+                      />
+                    ))
+                  }
+                </Row>
+              </Col>
+              <Hasil />
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
